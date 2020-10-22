e666ad90644f0181fbf62d6369a00645a6471a66
simon <simon.hengchen@gmail.com>
2020-10-22 16:03:48 +0200

    adding tqdm and possibility to dl both collections at once

--- extract.py
+++ extract.py
@@ -2,30 +2,53 @@
 
 import sys
 import requests
+from tqdm import tqdm
+import os
 
 coll = sys.argv[1]
 
+
+def download(coll,nrange,base):
+	print("Downloading",coll)
+	for n in tqdm(nrange):
+		url = f"http://www.academieroyale.be/Academie/documents/FichierPDF{base}{n}.pdf"
+		r = requests.get(url)
+		if os.path.exists(f"data/{coll}") == False:
+			os.mkdir(f"data/{coll}")
+		if r.status_code == 200:
+			with open(f"data/{coll}/{n}.pdf", "wb") as f:
+				f.write(r.content)
+		else:
+			print(f"{n}: {r.status_code}")
+
+	if coll == "nbn": # handle extra file
+		url = "https://www.academieroyale.be/Academie/documents/ALBIMOORWilly18211.pdf"
+		r = requests.get(url)
+		with open(f"data/nbn/2112.pdf", "wb") as f:
+			f.write(r.content)
+
 if coll == "bn":
-    nrange = range(2037, 2103)
-    base = "BiographieNationaleTome"
+	nrange = range(2037, 2103)
+	base = "BiographieNationaleTome"
+	download(coll,nrange,base)
+	
 elif coll == "nbn":
-    nrange = range(2103, 2112)
-    base = "NouvelleBiographieNational"
+	nrange = range(2103, 2112)
+	base = "NouvelleBiographieNational"
+	download(coll,nrange,base)
+	
+elif coll == "all":
+	coll = "bn"
+	nrange = range(2037, 2103)
+	base = "BiographieNationaleTome"
+	download(coll,nrange,base)
+	
+	coll = "nbn"
+	base = "NouvelleBiographieNational"
+	download(coll,nrange,base)
+
+	
 else:
-    print("Unknown collection")
-    sys.exit()
+	print("Unknown collection")
+	sys.exit()
 
-for n in nrange:
-    url = f"http://www.academieroyale.be/Academie/documents/FichierPDF{base}{n}.pdf"
-    r = requests.get(url)
-    if r.status_code == 200:
-        with open(f"data/{coll}/{n}.pdf", "wb") as f:
-            f.write(r.content)
-    else:
-        print(f"{n}: {r.status_code}")
-
-if coll == "nbn": # handle extra file
-    url = "https://www.academieroyale.be/Academie/documents/ALBIMOORWilly18211.pdf"
-    r = requests.get(url)
-    with open(f"data/nbn/2112.pdf", "wb") as f:
-        f.write(r.content)
